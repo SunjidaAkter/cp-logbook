ecf776d4304f3479a1f4313795c2f5bb3f9d3c47
SunjidaAkter <sanjidabintesayid7@gmail.com>
2026-09-04 22:49:26 +0600

    mission accomplished 🔥🚀🔥

---
+++ 2026-september/practices/codeforces/B_Everything_Everywhere.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define nl '\n'
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+#define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+typedef pair<ll,ll>pii;
+void solve(){
+    ll n;cin>>n;
+    vector<ll>v(n);
+    for(ll i=0;i<n;i++)cin>>v[i];
+    ll sum=0;
+    for(ll i=1;i<n;i++){
+        if(abs(v[i-1]-v[i])==1||((min(v[i],v[i-1])%abs(v[i]-v[i-1])==0)&&(max(v[i],v[i-1])%abs(v[i]-v[i-1])==0)))sum++;
+    }
+    cout<<sum<<nl;
+}
+int main(){
+    FAST;
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
